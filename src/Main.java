0d357074502375db08cbcc0a016813b123f14022
Pablos <pablos@list.ru>
2022-06-08 14:56:19 +0300

    in aaaabbbcdd, out - a4b3cd2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrom("казак"));
+        System.out.println(compressor("aaaabbbcdd"));
+        System.out.println("a4b3cd2");
     }
 
-    public static boolean isPalindrom(String str){
-        int len = str.length();
-        for (int i = 0; i < len/2; i++) {
-            if(str.charAt(i) != str.charAt(len-1-i)) return false;
+    static String compressor(String str) {
+        char ch = str.charAt(0);
+        int count = 1;
+        String ans = "";
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+
+            } else {
+                ans = count == 1 ? ans + Character.toString(ch) : ans + Character.toString(ch) + count;
+                ch = str.charAt(i);
+                count = 1;
+            }
         }
-        return true;
+        ans = count == 1 ? ans + Character.toString(ch) : ans + Character.toString(ch) + count;
+        return ans;
     }
 }
