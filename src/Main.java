0e0c5e4e54458a88f4fcb97e450dfc765256cba4
Pablos <pablos@list.ru>
2022-06-08 14:05:09 +0300

    isPalindrom

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        int N = 2; //длина числа
-        int K = 3; //количество цифр
-        int[] arr = new int[N];
-        List res = new ArrayList();
-        doIt(arr, 0, K, res);
-        System.out.println(Arrays.toString(res.toArray()));
+        System.out.println(isPalindrom("казак"));
     }
 
-    public static void doIt(int[] arr, int index, int K, List answer) {
-        if (index == arr.length) {
-            answer.add(Arrays.toString(arr));
-            return;
+    public static boolean isPalindrom(String str){
+        int len = str.length();
+        for (int i = 0; i < len/2; i++) {
+            if(str.charAt(i) != str.charAt(len-1-i)) return false;
         }
-        for (int i = 0; i < K; i++) {
-            arr[index] = i;
-            doIt(arr, index + 1, K, answer);
-        }
+        return true;
     }
 }
