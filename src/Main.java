b2285ac02094ef7327c2c3688834a60a093a5a31
Pablos <pablos@list.ru>
2022-06-08 12:39:20 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static void doIt(int[] arr, int index, int K){
+
+    }
+}
