a791fac8a3a9381e1b8078fa91a1d96b3408cffc
Pablos <pablos@list.ru>
2022-06-08 13:40:40 +0300

    For N = 2, K = 3 answer "00 01 02 10 11 12 20 21 22"

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int N = 2; //длина числа
+        int K = 3; //количество цифр
+        int[] arr = new int[N];
+        List res = new ArrayList();
+        doIt(arr, 0, K, res);
+        System.out.println(Arrays.toString(res.toArray()));
     }
-    public static void doIt(int[] arr, int index, int K){
 
+    public static void doIt(int[] arr, int index, int K, List answer) {
+        if (index == arr.length) {
+            answer.add(Arrays.toString(arr));
+            return;
+        }
+        for (int i = 0; i < K; i++) {
+            arr[index] = i;
+            doIt(arr, index + 1, K, answer);
+        }
     }
 }
