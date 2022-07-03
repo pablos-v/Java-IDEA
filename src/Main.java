7cbae3871f68fe140a2c93c4f43bff3d169158ec
Pablos <pablos@list.ru>
2022-07-03 14:43:00 +0300

    Wave algorythm

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,7 @@
 import java.util.*;
 
 public class Main {
-    static Map<String, Integer> priorityMap = Map.of("+", 1, "-", 1, "*", 2, "/", 2, "(", 0);
-
     public static void main(String[] args) {
-        Main solution = new Main();
-        System.out.println(solution.postfixFrom("2 * ( 3 + 5 ) * 4")); // 2 3 5 + * 4 *
-        System.out.println(solution.evalRPN(solution.postfixFrom("2 * ( 3 + 5 ) * 4").split(" ")));
-    }
 
-    public String postfixFrom(String infixExp) {
-        StringBuilder sb = new StringBuilder();
-        Deque<String> stack = new ArrayDeque<>();
-        String[] tokens = infixExp.split(" ");
-        for (String token : tokens) {
-            if (token.equals("(")) {
-                stack.push(token);
-            } else if (token.equals(")")) {
-                while (!stack.peek().equals("(")) {
-                    sb.append(stack.pop()).append(" ");
-                }
-                stack.pop();
-            } else if (!priorityMap.containsKey(token)) {
-                sb.append(token).append(" ");
-            } else {
-                if (!stack.isEmpty()) {
-                    while (!stack.isEmpty() && priorityMap.get(stack.peek()) >= priorityMap.get(token)) {
-                        sb.append(stack.pop()).append(" ");
-                    }
-                    stack.push(token);
-                }else {
-                    stack.push(token);
-                }
-            }
-        }
-        while (!stack.isEmpty()) sb.append(stack.pop()).append(" ");
-        return sb.toString().trim();
     }
-    public int evalRPN(String[] tokens) {
-        if (tokens.length == 1) {
-            return Integer.parseInt(tokens[0]);
-        }
-        int result = 0;
-        Stack<Integer> nums = new Stack<>();
-        Set<String> operations = Set.of("+", "-", "*", "/");
-        for (String token : tokens) {
-            if (operations.contains(token)) {
-                int second = nums.pop();
-                int first = nums.pop();
-                nums.add(perform(token, first, second));
-                result = nums.peek();
-            } else {
-                nums.add(Integer.parseInt(token));
-            }
-        }
-        return result;
-    }
-
-    int perform(String operator, int num1, int num2) {
-        switch (operator) {
-            case "+":
-                return num1 + num2;
-            case "-":
-                return num1 - num2;
-            case "*":
-                return num1 * num2;
-            case "/":
-                return num1 / num2;
-            default:
-                throw new RuntimeException();
-        }
-    }
-
 }
--- src/Wave.java
+++ src/Wave.java
@@ -1,181 +1,69 @@
-//public class Wave {
-//}
-//package Workshop;
-//
-//        import java.util.ArrayList;
-//        import java.util.LinkedList;
-//        import java.util.Queue;
-//
-//public class Program {
-//    public static void main(String[] args) {
-//
-//        var mg = new MapGenerator();
-//        System.out.println(
-//                new MapPrinter().rawData(
-//                        mg.getMap())
-//
-//        );
-//
-//        var lee = new WaveAlgorithm(mg.getMap());
-//        lee.Colorize(new Point2D(3, 3));
-//
-//        System.out.println(
-//                new MapPrinter().rawData(
-//                        mg.getMap())
-//
-//        );
-//    }
-//}
-//
-//class Point2D {
-//    int x, y;
-//
-//    public Point2D(int x, int y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//
-//    public int getX() {
-//        return x;
-//    }
-//
-//    public int getY() {
-//        return y;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format("x: %d  y: %d", x, y);
-//    }
-//}
-//
-//class MapGenerator {
-//    int[][] map;
-//
-//    public MapGenerator() {
-//        int[][] map = {
-//                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
-//                { -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, -1, -1, -1, -1, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, -1 },
-//                { -1, -1, -1, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, -1, 0, 0, -1, -1, -1, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, -1 },
-//                { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 },
-//                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
-//        };
-//
-//        this.map = map;
-//    }
-//
-//    public int[][] getMap() {
-//        return map;
-//    }
-//
-//    public void setCat(Point2D pos) {
-//        map[pos.x][pos.y] = -2;
-//    }
-//
-//    public void setExit(Point2D pos) {
-//        map[pos.x][pos.y] = -3;
-//    }
-//}
-//
-//class MapPrinter {
-//
-//    public MapPrinter() {
-//    }
-//
-//    public String rawData(int[][] map) {
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int row = 0; row < map.length; row++) {
-//            for (int col = 0; col < map[row].length; col++) {
-//                sb.append(String.format("%5d", map[row][col]));
-//            }
-//            sb.append("\n");
-//        }
-//        for (int i = 0; i < 3; i++) {
-//            sb.append("\n");
-//        }
-//
-//        return sb.toString();
-//    }
-//
-//    public String mapColor(int[][] map) {
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int row = 0; row < map.length; row++) {
-//            for (int col = 0; col < map[row].length; col++) {
-//                switch (map[row][col]) {
-//                    case 0:
-//                        sb.append("░");
-//                        break;
-//                    case -1:
-//                        sb.append("▓");
-//                        break;
-//                    case -2:
-//                        sb.append("К");
-//                        break;
-//                    case -3:
-//                        sb.append("E");
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-//            sb.append("\n");
-//        }
-//        for (int i = 0; i < 3; i++) {
-//            sb.append("\n");
-//        }
-//        return sb.toString();
-//    }
-//}
-//
-//class WaveAlgorithm {
-//    int[][] map;
-//
-//    public WaveAlgorithm(int[][] map) {
-//        this.map = map;
-//    }
-//
-//    public void Colorize(Point2D startPoint) {
-//        Queue<Point2D> queue = new LinkedList<Point2D>();
-//        queue.add(startPoint);
-//        map[startPoint.x][startPoint.y] = 1;
-//
-//        while (queue.size() != 0) {
-//            Point2D p = queue.remove();
-//
-//            if (map[p.x - 1][p.y] == 0) {
-//                queue.add(new Point2D(p.x - 1, p.y));
-//                map[p.x - 1][p.y] = map[p.x][p.y] + 1;
-//            }
-//            if (map[p.x][p.y - 1] == 0) {
-//                queue.add(new Point2D(p.x, p.y - 1));
-//                map[p.x][p.y - 1] = map[p.x][p.y] + 1;
-//            }
-//            if (map[p.x + 1][p.y] == 0) {
-//                queue.add(new Point2D(p.x + 1, p.y));
-//                map[p.x + 1][p.y] = map[p.x][p.y] + 1;
-//            }
-//            if (map[p.x][p.y + 1] == 0) {
-//                queue.add(new Point2D(p.x, p.y + 1));
-//                map[p.x][p.y + 1] = map[p.x][p.y] + 1;
-//            }
-//        }
-//    }
-//
-//    public ArrayList<Point2D> getRoad(Point2D exit) {
-//        ArrayList<Point2D> road = new ArrayList<>();
-//        ///
-//        return road;
-//    }
-//}
+import java.util.ArrayDeque;
+
+public class Wave {
+
+    public static int[][] mapGenerator(int value) {
+
+        int[][] map1 = {
+                {1, 0, -1},
+                {0, 0, 0},
+                {-1, 0, 0},
+
+        };
+        int[][] map2 = {
+                {1, -1, -1, -1, -1, -1},
+                {0, 0, 0, 0, -1, 0},
+                {-1, 0, 0, 0, 0, 0},
+                {-1, 0, -1, 0, -1, 0},
+                {-1, 0, 0, 0, -1, 0},
+                {-1, 0, 0, 0, -1, 0}
+        };
+        return value == 1 ? map1 : map2;
+    }
+
+    public static class WaveAlgorithm {
+        int[][] map;
+        int[][] movesArray = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        public WaveAlgorithm(int[][] map) {
+            this.map = map;
+        }
+
+        public boolean pointInMap(int x, int y) {
+            return x <= map[0].length - 1 && x >= 0 && y >= 0 && y <= map.length - 1;
+        }
+
+        public void Find() {
+            var q = new ArrayDeque<>();
+            int[] startPoint = {0, 0};
+            q.add(startPoint);
+            while (!q.isEmpty()) {
+                startPoint = (int[]) q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int x = startPoint[0] + movesArray[i][0];
+                    int y = startPoint[1] + movesArray[i][1];
+                    if (pointInMap(x, y) && map[x][y] == 0) {
+                        map[x][y] = map[startPoint[1]][startPoint[0]] + 1;
+                        q.add(new int[]{y, x});
+                    }
+                }
+            }
+        }
+    }
+
+    public static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        WaveAlgorithm wave = new WaveAlgorithm(mapGenerator(2));
+        wave.Find();
+        System.out.println(wave.map[wave.map.length - 1][wave.map[0].length - 1]);
+        print(wave.map);
+    }
+}
