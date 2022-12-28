f5606e2d7976b0a2dfe00be32de22a321ae25f4f
Pablos <pablos@list.ru>
2022-12-27 23:12:01 -0300

    RedBlackTree закостылил!

--- src/Homework/RedBlackTree.java
+++ src/Homework/RedBlackTree.java
@@ -3,32 +3,66 @@ package Homework;
 public class RedBlackTree {
     Node root;
 
+    public RedBlackTree() { // конструктор создаёт корневую чёрную ноду
+        this.root = new Node(null);
+        root.isRoot = true;
+        root.color = Colors.BLACK;
+    }
+
+    public enum Colors {
+        RED,
+        BLACK
+    }
+
     public class Node {
-        int value;
-        enum Color{
-            RED,
-            BLACK
-        }
+        Integer value;
+        boolean isRoot = false;
+        Colors color = Colors.RED;
         Node left, right;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
     }
 
-    public void insert(int value){
+    public void insert(int value) {
+        if (root.value == null) root.value = value;
+        else {
+            Node cur = root;
+
+            while (cur != null) {
+                if (value <= cur.value) {
+                    if (cur.left == null) {
+                        cur.left = new Node(value); // как вернуться?
+                        return;
+                    } else {
+                        cur = cur.left;
+                    }
+                } else {
+                    if (cur.right == null) {
+                        cur.right = new Node(value); // как вернуться?
+                        return;
+                    } else {
+                        cur = cur.right;
+                    }
+                }
+            }
+        }
+    }
+
+    public void balance() {
 
     }
 
-    public void balance(){
+    public void rotateLeft() {
 
     }
 
-    public void rotateLeft(){
+    public void rotateRight() {
 
     }
 
-    public void rotateRight(){
-
-    }
-
-    public void changeColor(){
+    public void changeColor() {
 
     }
 
@@ -41,7 +75,27 @@ public class RedBlackTree {
         return null;
     }
 
+    public void printTree() {
+        System.out.println(root.value);
+        System.out.println(root.left.value);
+        System.out.println(root.right.value);
+        System.out.println("---");
+        System.out.println(root.left.left.value);
+        System.out.println(root.left.right.value);
+        System.out.println(root.right.left.value);
+        System.out.println(root.right.right.value);
+    }
+
     public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(10);
+        tree.insert(13);
+        tree.insert(7);
+        tree.insert(11);
+        tree.insert(15);
+        tree.insert(5);
+        tree.insert(8);
+        tree.printTree();
 
     }
 
